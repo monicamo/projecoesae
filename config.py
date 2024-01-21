c0d99c4d9147e0d214afb2b2f57162ee1ac97520
Monica Monteiro <monica.monteiro@estadao.com>
2024-01-21 18:08:13 -0300

    Update Data

---
+++ config.py
@@ -0,0 +1,10 @@
+SECRET_KEY = 'alura'
+
+SQLALCHEMY_DATABASE_URI = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD = 'mysql+mysqlconnector',
+        usuario = 'root',
+        senha = 'biju',
+        servidor = 'localhost',
+        database = 'planilhateca'
+    )
---
+++ views.py
@@ -0,0 +1,85 @@
+from flask import render_template, request, redirect, session, flash, url_for
+from main import app, db
+from models import Planilhas, Usuarios
+
+
+@app.route('/')
+def index():
+    lista = Planilhas.query.order_by(Planilhas.id)
+    return render_template('lista.html', titulo='Planilhas', planilhas=lista)
+
+
+@app.route('/novo')
+def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
+    return render_template('novo.html', titulo='Nova Planilha')
+
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    planilha = Planilhas.query.filter_by(nome=nome).first()
+
+    if planilha:
+        flash('Planilha já existente!')
+        return redirect(url_for('index'))
+
+    novo_planilha = Planilhas(nome=nome, categoria=categoria, console=console)
+    db.session.add(novo_planilha)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+@app.route('/editar/<int:id>')
+def editar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('editar')))
+
+    planilha = Planilhas.query.filter_by(id=id).first()
+
+    return render_template('editar.html', titulo='Editando Planilha', planilha=planilha)
+
+
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+    planilha = Planilhas.query.filter_by(id=request.form['id']).first()
+    planilha.nome = request.form['nome']
+    planilha.categoria = request.form['categoria']
+    planilha.console = request.form['console']
+
+    db.session.add(planilha)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    usuario = Usuarios.query.filter_by(nickname=request.form['usuario']).first()
+    if usuario:
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname + ' logado com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
+    else:
+        flash('Usuário não logado.')
+        return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('index'))
